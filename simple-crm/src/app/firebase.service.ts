fb1501081a6e6fd8ee24ff1a36ff4940fd816447
Michel <woelfelm@t-online.de>
2023-11-19 17:24:11 +0100

    firestore

--- simple-crm/src/app/firebase.service.ts
+++ simple-crm/src/app/firebase.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CustomerData } from './interfaces/customer-dialog-interface';
 import { inject } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+import { Firestore, collection, doc, collectionData, onSnapshot } from '@angular/fire/firestore';
 
 
 @Injectable({
@@ -12,15 +12,77 @@ import { Firestore } from '@angular/fire/firestore';
 
 export class FirebaseService {
 
+
+
   firestore: Firestore = inject(Firestore);
+  customers: CustomerData[] = [];
+
+  items$;
+  items;
+
+  unsubList;
+  unsubSingle;
   ngOnInit() {
 
   }
- customers : CustomerData[] = [];
-  constructor() { 
- 
+
+  constructor() {
+
+    this.unsubList = this.subCustomerList();
+
+    this.unsubSingle = onSnapshot(this.getSingleDocRef("customers", "adsfasdf"), (element) => {
+    });
+
+
+    this.unsubSingle();
+
+
+
+    this.items$ = collectionData(this.getCustomersRef());
+    this.items = this.items$.subscribe((list) => {
+      list.forEach((element) => {
+        console.log(element);
+      })
+      this.items.unsubscribe();
+    });
+    
   }
 
- 
+  ngOnDestroy() {
+    // this.unsubList();
+   
+  }
 
+  subCustomerList() {
+   return onSnapshot(this.getCustomersRef(), (list) => {
+      list.forEach(element => {
+        console.log(this.setCustomerData(element.data()));
+      })
+    })
+  }
+
+  setCustomerData(obj:any) {
+    return {
+      name: obj.name || "",
+      company: obj.company || "",
+      address: obj.address || "",
+      zipcode: obj.zipcode|| "",
+    }
+  }
+
+
+
+  /**This is for getting the collection "customers" from firebase */
+  getCustomersRef() {
+    return collection(this.firestore, 'customers');
+
+  }
+  /**Here i get the Infos about a single customer, 
+  
+  * colId = the id from the collection, in this case probably "customers,"
+  * doc id = the specific id from a customer.
+  */
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
